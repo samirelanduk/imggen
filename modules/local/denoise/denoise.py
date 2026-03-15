64f74fdf0cf6c1db048616205709055e37f8483b
Sam Ireland <mail@samireland.com>
2026-03-15 15:16:54 +0000

    DENOISE output

--- modules/local/denoise/denoise.py
+++ modules/local/denoise/denoise.py
@@ -1,5 +1,7 @@
 import argparse
+import torch
 
+# Get paths
 parser = argparse.ArgumentParser(description="Denoise a latent image using a model.")
 parser.add_argument("latent", type=str, help="Path to the latent image file (.pt)")
 parser.add_argument("conditioning", type=str, help="Path to the conditioning file (.pt)")
@@ -8,3 +10,9 @@ args = parser.parse_args()
 latent = args.latent
 conditioning = args.conditioning
 model = args.model
+
+# Load image
+latent = torch.load(latent)
+
+# Save final image
+torch.save(latent, "denoised.pt")
