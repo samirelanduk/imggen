4a3ba0a388d9bd2a46145194fea417f5d5cc183d
Sam Ireland <mail@samireland.com>
2026-03-15 22:24:14 +0000

    Load blocks from st file

--- modules/local/denoise/denoise.py
+++ modules/local/denoise/denoise.py
@@ -1,4 +1,5 @@
 import argparse
+import safetensors
 import torch
 
 # Get paths
@@ -12,6 +13,29 @@ latent_path = args.latent
 conditioning_path = args.conditioning
 model_path = args.model
 steps = args.steps
+
+# Load model
+with safetensors.safe_open(model_path, framework="pt", device="cpu") as f:
+    tensors = {
+        "blocks": {
+            "input": {},
+            "middle": {},
+            "output": {},
+        }
+    }
+    for key in f.keys():
+        if key.startswith("model.diffusion_model."):
+            name = key[len("model.diffusion_model."):]
+            if name.startswith("input_blocks."):
+                name = name[len("input_blocks."):]
+                tensors["blocks"]["input"][name] = f.get_tensor(key)
+            elif name.startswith("middle_block."):
+                name = name[len("middle_block."):]
+                tensors["blocks"]["middle"][name] = f.get_tensor(key)
+            elif name.startswith("output_blocks."):
+                name = name[len("output_blocks."):]
+                tensors["blocks"]["output"][name] = f.get_tensor(key)
+
 
 # Load image
 latent = torch.load(latent_path)
